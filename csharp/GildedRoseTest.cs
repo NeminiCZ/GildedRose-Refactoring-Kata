a654b4972f2f9f333a85b4f9a5b7195b729be9c3
Martina Minátová <m.minatova@gmail.com>
2018-05-15 14:12:29 +0200

    added testNormalItemQualityExpired

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -30,6 +30,18 @@ namespace csharp
             Assert.AreEqual(1, items[0].Quality);
         }
 
+        [Test]
+        public void TestNormalItemQualityExpired()
+        {
+            var items = new List<Item> { new Item { Name = "bread", SellIn = 2, Quality = 0 } };
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+
+            Assert.AreEqual("bread", items[0].Name);
+            Assert.AreEqual(1, items[0].SellIn);
+            Assert.AreEqual(0, items[0].Quality);
+        }
+
 
     }
 }
