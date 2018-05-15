ce579e2db4b13d5a419604bc702f7512fef0476e
Martina Minátová <m.minatova@gmail.com>
2018-05-15 15:04:25 +0200

    added Backstage tests

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -104,5 +104,57 @@ namespace csharp
             Assert.AreEqual(80, items[0].Quality);
         }
 
+        [Test]
+        public void TestBackstagePassMore10Days()
+        {
+            var items = new List<Item> { new Item { Name = "Backstage passes to a wow concert", SellIn = 12, Quality = 10 } };
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual("Backstage passes to a wow concert", items[0].Name);
+            Assert.AreEqual(11, items[0].SellIn);
+            Assert.AreEqual(11, items[0].Quality);
+        }
+
+        [Test]
+        public void TestBackstagePassLess10Days()
+        {
+            var items = new List<Item> { new Item { Name = "Backstage passes to a wow concert", SellIn = 10, Quality = 10 } };
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual("Backstage passes to a wow concert", items[0].Name);
+            Assert.AreEqual(9, items[0].SellIn);
+            Assert.AreEqual(12, items[0].Quality);
+        }
+
+        [Test]
+        public void TestBackstagePassLess5Days()
+        {
+            var items = new List<Item> { new Item { Name = "Backstage passes to a wow concert", SellIn = 5, Quality = 10 } };
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual("Backstage passes to a wow concert", items[0].Name);
+            Assert.AreEqual(4, items[0].SellIn);
+            Assert.AreEqual(13, items[0].Quality);
+        }
+
+        [Test]
+        public void TestBackstagePassOld()
+        {
+            var items = new List<Item> { new Item { Name = "Backstage passes to a wow concert", SellIn = 0, Quality = 10 } };
+            var app = new GildedRose(items);
+
+            app.UpdateQuality();
+
+            Assert.AreEqual("Backstage passes to a wow concert", items[0].Name);
+            Assert.AreEqual(-1, items[0].SellIn);
+            Assert.AreEqual(0, items[0].Quality);
+        }
+
     }
 }
