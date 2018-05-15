ec6d9e79b24eb821c03bfd1b4b1ed798cffc4d71
Martina Minátová <m.minatova@gmail.com>
2018-05-15 14:03:21 +0200

    added testNormalItem

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -7,12 +7,15 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        public void testNormalItem()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
+            var items = new List<Item> { new Item { Name = "bread", SellIn = 2, Quality = 2 } };
+            var app = new GildedRose(items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+
+            Assert.AreEqual("bread", items[0].Name);
+            Assert.AreEqual(1, items[0].SellIn);
+            Assert.AreEqual(1, items[0].Quality);
         }
     }
 }
